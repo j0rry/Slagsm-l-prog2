8a0ae8a748d97d6dca74ac31eb95147614f0b087
Oscar Alshammar <oscar.alshammar@gmail.com>
2025-08-28 12:54:07 +0200

    Två förbättringar

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -2,36 +2,43 @@
 
 static void Fight()
 {
-
     int p1HP = 100;
     int p2HP = 100;
+    string p1Name = "";
+    string p2Name = "Kebab Kungen";
 
-    string p1Name = "Alex";
-    string p2Name = "Calin";
-
+    while (string.IsNullOrEmpty(p1Name))
+    {
+        Console.Write("Ange namn på spelare 1: ");
+        p1Name = Console.ReadLine();
+    }
 
     while (p1HP > 0 && p2HP > 0)
     {
-        Console.WriteLine($"{p1Name}: {p1HP}");
-        Console.WriteLine($"{p2Name}: {p2HP}");
-        p2HP -= Random.Shared.Next(10, 25);
-        p1HP -= Random.Shared.Next(10, 25);
+        int p1Damage = Random.Shared.Next(10, 25);
+        int p2Damage = Random.Shared.Next(10, 25);
+
+        p2HP -= p1Damage;
+        p1HP -= p2Damage;
+
+        Console.WriteLine($"{p1Name} gör {p1Damage} skada!");
+        Console.WriteLine($"{p2Name} gör {p2Damage} skada!");
+        Console.WriteLine($"{p1Name}: {Math.Max(p1HP, 0)} HP");
+        Console.WriteLine($"{p2Name}: {Math.Max(p2HP, 0)} HP");
+        Console.WriteLine("Tryck på Enter för nästa runda");
         Console.ReadLine();
     }
 
-    if (p1HP > 0)
+    if (p1HP > 0 && p2HP <= 0)
     {
         Console.WriteLine($"{p1Name} vann");
     }
-
-    if (p1HP <= 0 && p2HP <= 0)
-    {
-        Console.WriteLine($"Oavgjort!");
-    }
-
-    if (p2HP > 0)
+    else if (p1HP <= 0 && p2HP > 0)
     {
         Console.WriteLine($"{p2Name} vann");
     }
-
+    else
+    {
+        Console.WriteLine("Oavgjort!");
+    }
 }
